4de339f42b6badedd7899b3fae025ad51e77ee8d
unknown <lucas.montezumaa@gmail.com>
2025-09-24 16:49:28 -0300

    second commit

--- brisbane/settings.py
+++ brisbane/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-2k&&ip(3hd70tiy41d$ktka@^*5t^%88f!onk!z=0gtd_&9(up
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["brisbane.pythonanywhere.com", "www.brisbane.pythonanywhere.com"]
 
 
 # Application definition
